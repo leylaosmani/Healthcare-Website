09a0a78b9a0467b160a33f5f50f9beb612ca5b35
Leyla <Leyla.osmani@outlook.com>
2024-11-07 13:47:26 -0500

    First Commit

---
+++ accounts/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    ROLE_CHOICES = [
+        ('doctor', 'Doctor'),
+        ('nurse', 'Nurse'),
+        ('patient', 'Patient'),
+    ]
+#this links each profile to 1 user
+    user = models.OneToOneField(User, on_delete=models.CASCADE) 
+#this is where the roles are defined with 3 roles(choices)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
